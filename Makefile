2895abe0eb2612f62e1597181017213a3c1dd0a8
Petr Shatsillo <Pshatsillo@gmail.com>
2024-01-17 12:39:07 +0300

    Config app

--- Makefile
+++ Makefile
@@ -9,3 +9,4 @@ clean:
 util:
 	gcc DALI_app.c -o dali_send
 	gcc DALI_init.c parson.c -o dali_init
+	gcc dali_config.c parson.c -o dali_config
--- dali_config.c
+++ dali_config.c
@@ -54,6 +54,136 @@ void init_search(int fd)
     send_command(fd, "A500");
     usleep(90000);
 }
+int count_bits(unsigned x)
+{
+    int bits = 0;
+    while (x)
+    {
+        if (x & 1U)
+            bits++;
+        x >>= 1;
+    }
+    return bits;
+}
+
+int int_bits(void)
+{
+    return count_bits(~0U);
+}
+
+char *print_nbits(unsigned x, unsigned n)
+{
+    char *out = malloc(n);
+    int i = int_bits();
+    i = (n < i) ? n - 1 : i - 1;
+    for (; i >= 0; i--)
+    {
+        if ((x >> i) & 1U)
+        {
+            out[(n - 1) - i] = '1';
+        }
+        else
+        {
+            out[(n - 1) - i] = '0';
+        }
+    }
+    return out;
+}
+
+int loadConfig()
+{
+    if (stat(filename, &filestatus) != 0)
+    {
+        fptr = fopen("config.json", "w");
+        JSON_Value *root_value = json_value_init_object();
+        JSON_Object *root_object = json_value_get_object(root_value);
+        char *serialized_string = NULL;
+        json_object_set_value(root_object, "devices", json_parse_string("[]"));
+
+        JSON_Value *device_value = json_value_init_object();
+        JSON_Object *device_object = json_value_get_object(device_value);
+        for (u_int8_t i = 0; i <= 15; i++)
+        {
+            char *name;
+            asprintf(&name, "%i", i);
+            json_object_set_value(device_object, name, json_parse_string("[{\"Device\":99}]"));
+            free(name);
+        }
+
+        json_object_set_value(root_object, "group", device_value);
+        serialized_string = json_serialize_to_string_pretty(root_value);
+        fprintf(fptr, serialized_string);
+        fclose(fptr);
+        free(file_contents);
+        stat(filename, &filestatus);
+    }
+    else
+    {
+        hasFile = true;
+    }
+    file_size = filestatus.st_size;
+    file_contents = (char *)malloc(filestatus.st_size);
+    if (file_contents == NULL)
+    {
+        fprintf(stderr, "Memory error: unable to allocate %d bytes\n", file_size);
+        return 1;
+    }
+
+    fptr = fopen(filename, "rt");
+    if (fread(file_contents, file_size, 1, fptr) != 1)
+    {
+        fprintf(stderr, "Unable to read content of %s\n", filename);
+        fclose(fptr);
+        free(file_contents);
+        return 1;
+    }
+    fclose(fptr);
+    user_data = json_parse_string(file_contents);
+    free(file_contents);
+}
+
+void registerGroups()
+{
+    char buffer[7];
+    JSON_Object *deviceJson = json_object(user_data);
+    JSON_Object *grpArr = json_object_get_object(deviceJson, "group");
+    size_t i;
+    for (i = 0; i < json_object_get_count(grpArr); i++)
+    {
+        char *name;
+        asprintf(&name, "%i", i);
+        JSON_Value *grpName = json_object_get_value(grpArr, name);
+        JSON_Array *gArr = json_array(grpName);
+        free(name);
+        printf("Group %d id: ", i);
+        for (size_t j = 0; j < json_array_get_count(gArr); j++)
+        {
+            JSON_Object *groupDev = json_array_get_object(gArr, j);
+            u_int8_t ShortAddr = json_object_get_number(groupDev, "Device");
+            if (ShortAddr <= 64)
+            {
+                char *resp = print_nbits(ShortAddr, 6);
+                char *devID = malloc(6);
+                strcpy(devID, resp);
+                free(resp);
+                char *respG = print_nbits(i, 4);
+                char *GrpAddr = malloc(4);
+                strcpy(GrpAddr, respG);
+                free(respG);
+                char *longAddr = NULL;
+                asprintf(&longAddr, "0%s10110%s", devID, GrpAddr);
+                long hexval = strtol(longAddr, NULL, 2);
+                sprintf(buffer, "%04x", hexval);
+                send_command(fd, &buffer[0]);
+                usleep(90000);
+                send_command(fd, &buffer[0]);
+                usleep(90000);
+                printf("%d ", ShortAddr);
+            }
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -73,4 +203,5 @@ int main(int argc, char *argv[])
     }
 
     init_search(fd);
+    registerGroups();
 }
